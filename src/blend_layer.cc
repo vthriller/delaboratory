308f9b4889f77b177c1ad170672e320207dab10f
jacekpoplawski <jacekpoplawski@gmail.com>
2011-07-16 16:26:51 +0000

    initial save support for blend mask

--- src/blend_layer.cc
+++ src/blend_layer.cc
@@ -137,6 +137,7 @@ void deBlendLayer::saveSpecific(xmlNodePtr node)
 {
     alpha.save(node, "alpha");
     blendMode.save(node, "blend_mode");
+    blendMask.save(node, "blend_mask");
     channels.save(node, "channels");
     overlayLayer.save(node, "overlay_layer");
 }
@@ -157,6 +158,11 @@ void deBlendLayer::loadSpecific(xmlNodePtr node)
         if ((!xmlStrcmp(child->name, xmlCharStrdup("blend_mode")))) 
         {
             blendMode.load(child);
+        }
+
+        if ((!xmlStrcmp(child->name, xmlCharStrdup("blend_mask")))) 
+        {
+            blendMask.load(child);
         }
 
         if ((!xmlStrcmp(child->name, xmlCharStrdup("channels")))) 
