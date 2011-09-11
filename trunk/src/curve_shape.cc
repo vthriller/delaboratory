8f877727c234f800d916aea6df9f065ebdb748b4
jacekpoplawski@gmail.com <jacekpoplawski@gmail.com@4e2834a2-6c7f-4a28-f73c-28c74d6238bb>
2011-09-11 19:12:24 +0000

    fully working Bezier curves

--- trunk/src/curve_shape.cc
+++ trunk/src/curve_shape.cc
@@ -21,7 +21,7 @@
 #include "curve_function_spline.h"
 #include "curve_function_bezier.h"
 
-#define BEZIER 0
+#define BEZIER 1
 
 deCurveShape::deCurveShape(int _size)
 :size(_size)
@@ -233,26 +233,78 @@ void deCurveShape::generateSpline()
 
 void deCurveShape::generateBezier()
 {
+    /* math formulas from GIMP 
+       gimp_curve_plot from gimpcurve.c */
+
     deNodes::const_iterator i;
     
     for (i = nodes.begin(); i != nodes.end(); i++)
     {
+        bool left = false;
+        bool right = false;
+
         deValue x0 = (*i).first;
         deValue y0 = (*i).second;
+        deNodes::const_iterator j = i;
         deNodes::const_iterator k = i;
+        j++;
         k++;
         deValue x3 = x0;
         deValue y3 = y0;
-        if (k != nodes.end())
+        if (j != nodes.end())
         {
-            x3 = (*k).first;
-            y3 = (*k).second;
+            x3 = (*j).first;
+            y3 = (*j).second;
+            k++;
+            if (k != nodes.end())
+            {
+                right = true;
+            }
         }
+
+        deNodes::const_iterator h = i;
+        if (h != nodes.begin())
+        {
+            h--;
+            left = true;
+        }            
+
         deValue dx = x3 - x0;
         deValue dy = y3 - y0;
 
-        deValue y1 = y0 + dy / 3.0;
-        deValue y2 = y0 + dy * 2.0 / 3.0;
+        deValue y1;
+        deValue y2;
+
+        if ((!left) && (!right))
+        {
+            y1 = y0 + dy / 3.0;
+            y2 = y0 + dy * 2.0 / 3.0;
+        }
+
+        if ((!left) && (right))
+        {
+            deValue s = ((*k).second - y0) / ((*k).first - x0);
+            y2 = y3 - s * dx / 3.0;
+            y1 = y0 + (y2 - y0) / 2.0;
+        }
+
+        if ((left) && (!right))
+        {
+            deValue s = (y3 - (*h).second) / (x3 - (*h).first);
+
+            y1 = y0 + s * dx / 3.0;
+            y2 = y3 + (y1 - y3) / 2.0;
+        }
+
+        if ((left) && (right))
+        {
+            deValue s1 = (y3 - (*h).second) / (x3 - (*h).first);
+            deValue s2 = ((*k).second - y0) / ((*k).first - x0);
+
+            y1 = y0 + s1 * dx / 3.0;
+            y2 = y3 - s2 * dx / 3.0;
+        }
+
 
         int p1 = x0 * (size - 1);
         int p2 = x3 * (size - 1);
