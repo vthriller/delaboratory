76038b1fe5802876374f226bb8d7547867838c3e
jacekpoplawski <jacekpoplawski@gmail.com>
2011-11-15 02:08:38 +0000

    property choice ui

---
+++ src/property_choice_ui.h
@@ -0,0 +1,42 @@
+/*
+    delaboratory - color correction utility
+    Copyright (C) 2011 Jacek Poplawski
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _DE_PROPERTY_CHOICE_UI_H
+#define _DE_PROPERTY_CHOICE_UI_H
+
+#include "choice.h"
+class deLayer;
+class dePropertyChoice;
+
+class dePropertyChoiceUI:public deChoice
+{
+    private:
+        dePropertyChoice& property;
+        deLayer& layer;
+
+    public:
+        dePropertyChoiceUI(wxWindow *parent, dePropertyChoice& _property, deLayer& _layer);
+        virtual ~dePropertyChoiceUI();
+
+        virtual void onChoose(int c);
+
+        void setFromProperty();
+
+};        
+
+#endif
