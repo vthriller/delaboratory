dbbef9adfc3098b458e06535669701b22c6f4bc8
jacekpoplawski@gmail.com <jacekpoplawski@gmail.com@4e2834a2-6c7f-4a28-f73c-28c74d6238bb>
2012-01-07 06:48:44 +0000

    initial version of vignette layer

---
+++ trunk/src/vignette.cc
@@ -0,0 +1,60 @@
+/*
+    delaboratory - color correction utility
+    Copyright (C) 2011 Jacek Poplawski
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "vignette.h"
+#include <cmath>
+#include <cassert>
+
+void vignetteChannel(deValue* destination, deSize size, deValue centerX, deValue centerY, deValue radiusX, deValue radiusY)
+{
+    assert(source);
+    assert(destination);
+
+    int w = size.getW();
+    int h = size.getH();
+
+    deValue ww = w / 2.0;
+    deValue hh = h / 2.0;
+
+    assert(w > 0);
+    assert(h > 0);
+    assert(radius > 0);
+
+    int i;
+    int j;
+
+    int p = 0;
+    for (i = 0; i < h; i++)
+    {
+        deValue y = (i - hh) / hh;
+        for (j = 0; j < w; j++)
+        {
+            deValue x = (j - ww) / ww;
+            deValue r = sqrt(x * x + y * y) / 1.0;
+            deValue v = 1.0 - r;
+            if (v < 0)
+            {
+                v = 0;
+            }
+            destination[p] = v;
+            p++;
+        }
+    }
+
+}
+
---
+++ trunk/src/vignette_layer.h
@@ -0,0 +1,54 @@
+/*
+    delaboratory - color correction utility
+    Copyright (C) 2011 Jacek Poplawski
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _DE_VIGNETTE_LAYER_H
+#define _DE_VIGNETTE_LAYER_H
+
+#include "action_layer.h"
+#include "property_value.h"
+
+class deVignetteLayer:public deActionLayer
+{
+    private:
+        dePropertyValue centerX;
+        dePropertyValue centerY;
+        dePropertyValue radiusX;
+        dePropertyValue radiusY;
+
+    protected:
+        virtual bool singleChannelProcessing() const {return true;};
+        virtual std::string getType() const {return "vignette";};
+
+    public:
+        deVignetteLayer(deColorSpace _colorSpace, int _index, int _sourceLayer, deLayerStack& _layerStack, deLayerProcessor& _layerProcessor, deChannelManager& _channelManager, deViewManager& _viewManager, const std::string& _name);
+        virtual ~deVignetteLayer();
+
+        virtual bool isChannelNeutral(int index);
+
+        virtual void processAction(int i, const deChannel& sourceChannel, deChannel& channel, deSize size);
+
+        virtual void load(xmlNodePtr root);
+        virtual void save(xmlNodePtr root);
+
+        virtual std::string getActionName() {return "setup";};
+
+        dePropertyValue& getPropertyRadiusX() {return radiusX;};
+
+};
+
+#endif
