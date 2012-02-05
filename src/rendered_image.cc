0ff071e821641d1f21f1daac9523ba4a1c248dd0
jacekpoplawski <jacekpoplawski@gmail.com>
2012-02-05 21:28:29 +0000

    started rendered image

--- src/rendered_image.cc
+++ src/rendered_image.cc
@@ -16,36 +16,13 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef _DE_RENDERER_H
-#define _DE_RENDERER_H
-
-class wxDC;
-class wxImage;
-class deChannelManager;
-class deViewManager;
-class deLayerProcessor;
-class deLayerStack;
-#include <wx/wx.h>
-#include "size.h"
 #include "rendered_image.h"
 
-class deRenderer
+deRenderedImage::deRenderedImage()
 {
-    private:
-        deRenderedImage renderedImage;
-        wxImage* image;
-        deSize size;
-        deChannelManager& channelManager;
-        wxMutex mutex;
+}
 
-        unsigned char* getCurrentImageData();
+deRenderedImage::~deRenderedImage()
+{
+}
 
-    public:
-        deRenderer(deChannelManager& _channelManager);
-        virtual ~deRenderer();
-
-        bool render(wxDC& dc);
-        bool prepareImage(const deViewManager& viewManager, deLayerProcessor& layerProcessor, deLayerStack& layerStack);
-};
-
-#endif
