b402baa16737f44e560558bca4d1edef5e8c9217
jacekpoplawski <jacekpoplawski@gmail.com>
2012-02-07 20:54:07 +0000

    improved scaling

--- src/scale_channel.cc
+++ src/scale_channel.cc
@@ -21,128 +21,64 @@
 #include <iostream>
 #include <cassert>
 
-/*
-void scaleChannel(const deChannel& s, deChannel& d, const deSize& ss, const deSize& ds)
+void scaleChannel(deValue* src, deValue* dst, int maxx, int ox, deValue scaleW, int maxy, int oy, deValue scaleH, int ws, int hs, int wd)
 {
-    int ws = ss.getW();
-    int hs = ss.getH();
-    int wd = ds.getW();
-    int hd = ds.getH();
-    float scaleW = (float) ws / wd;
-    float scaleH = (float) hs / hd;
-
-    const deValue* src = s.getPixels();
-    deValue* dst = d.getPixels();
+    if (ws < 1)
+    {
+        return;
+    }
+    if (hs < 1)
+    {
+        return;
+    }
 
     int x;
-    int y;
-    for (x = 0; x < wd; x++)
-    {
-        for (y = 0; y < hd; y++)
-        {
-            int xx = scaleW * x;
-            int yy = scaleH * y;
-            deValue v = src[yy*ws+xx];
-            dst[y*wd+x] = v;
-        }
-    }
-}
-
-void scaleChannel(const deChannel& s, deChannel& d, const deSize& ss, const deSize& ds, deValue scale, deValue offsetX, deValue offsetY)
-{
-
-    deValue centerX = (offsetX + 1.0) / 2.0;
-    deValue centerY = (offsetY + 1.0) / 2.0;
-
-    int ws = ss.getW();
-    int hs = ss.getH();
-    deValue sAspect = ss.getAspect();
-    deValue dAspect = ds.getAspect();
-
-    centerX *= ws;
-    centerY *= hs;
-
-    int wd = ds.getW();
-    int hd = ds.getH();
-
-    float scaleW = (float) ws / wd;
-    float scaleH = (float) hs / hd;
-
-    scaleH *= (sAspect / dAspect);
-
-    scaleW /= scale;
-    scaleH /= scale;
-
-    int ox = centerX - scaleW * (wd / 2);
-    int oy = centerY - scaleH * (hd / 2);
-
-    int maxox = ws - scaleW * ( wd - 1);
-    int maxoy = hs - scaleH * ( hd - 1);
-
-    if (ox > maxox)
-    {
-        ox = maxox;
-    }
-    if (oy > maxoy)
-    {
-        oy = maxoy;
-    }
-    if (ox < 0)
-    {
-        ox = 0;
-    }
-    if (oy < 0)
-    {
-        oy = 0;
-    }
-
-    int maxy = hd;
-    int maxyy = (hs - oy) / scaleH;
-    if (maxyy < maxy)
-    {
-        maxy = maxyy;
-    }
-
-    int maxx = wd;
-    int maxxx = (ws - ox) / scaleW;
-    if (maxxx < maxx)
-    {
-        maxx = maxxx;
-    }
-
-    const deValue* src = s.getPixels();
-    deValue* dst = d.getPixels();
-
-    int x;
-    int y;
     for (x = 0; x < maxx; x++)
     {
-        int xx = ox + scaleW * x;
-        assert(xx < ws);
-        assert(xx >= 0);
-        for (y = 0; y < maxy; y++)
+        int x1 = ox + scaleW * x;
+        int x2 = ox + scaleW * (x + 1);
+        if (x2 == x1)
         {
-            int yy = oy + scaleH * y;
-            assert(yy < hs);
-            assert(yy >= 0);
-            deValue v = src[yy*ws+xx];
-            dst[y*wd+x] = v;
+            x2++;
         }
-    }
-}
-*/
+        if (x2 > ws)
+        {
+            x2 = ws;
+        }
 
-void scaleChannel(deValue* src, deValue* dst, int maxx, int ox, deValue scaleW, int maxy, int oy, deValue scaleH, int ws, int wd)
-{
-    int x;
-    int y;
-    for (x = 0; x < maxx; x++)
-    {
-        int xx = ox + scaleW * x;
+        int y;
+
         for (y = 0; y < maxy; y++)
         {
-            int yy = oy + scaleH * y;
-            deValue v = src[yy*ws+xx];
+            int y1 = oy + scaleH * y;
+            int y2 = oy + scaleH * (y + 1);
+            if (y2 == y1)
+            {
+                y2++;
+            }
+
+            if (y2 > hs)
+            {
+                y2 = hs;
+            }
+
+            int x0;
+            int y0;
+
+            deValue value = 0.0;
+            int n = 0;
+
+            for (x0 = x1; x0 < x2; x0++)
+            {
+                for (y0 = y1; y0 < y2; y0++)
+                {
+                    value += src[x0 + y0 * ws];
+                    n++;
+                }
+            }
+
+            deValue v = value / n;
+
             dst[y*wd+x] = v;
         }
     }
