5cc90b24c09b32ce90c367423ce381072a8eea73
jacekpoplawski@gmail.com <jacekpoplawski@gmail.com@4e2834a2-6c7f-4a28-f73c-28c74d6238bb>
2012-02-17 22:02:38 +0000

    raw module

---
+++ trunk/src/raw_module.cc
@@ -0,0 +1,53 @@
+/*
+    delaboratory - color correction utility
+    Copyright (C) 2011 Jacek Poplawski
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "raw_module.h"
+#include "dcraw_support.h"
+#include "image_io.h"
+#include "str.h"
+
+deRawModule::deRawModule()
+{
+    dcraw_version = "";
+}
+
+deRawModule::~deRawModule()
+{
+}
+
+void deRawModule::onInit()
+{
+    dcraw_version = checkDcraw();
+}    
+
+std::string deRawModule::getVersion() const
+{
+    return dcraw_version;
+}
+
+bool deRawModule::loadRAW(const std::string& fileName, deStaticImage& image)
+{
+    std::string tmp = getTmp() + "delaboratory.ppm";
+
+    if (!execDcraw(fileName, tmp))
+    {
+        return false;
+    }
+
+    return loadPPM(tmp, image, deColorSpaceLAB);
+}
