c511813734c041f5212636450d1fbbe2bf4758ac
jacekpoplawski <jacekpoplawski@gmail.com>
2012-02-17 22:02:38 +0000

    raw module

---
+++ src/raw_module.h
@@ -0,0 +1,45 @@
+/*
+    delaboratory - color correction utility
+    Copyright (C) 2011 Jacek Poplawski
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _DE_RAW_MODULE_H
+#define _DE_RAW_MODULE_H
+
+#include <string>
+class deStaticImage;
+
+class deRawModule
+{
+    private:
+        deRawModule(const deRawModule&);
+        deRawModule& operator =(const deRawModule&);
+
+        std::string dcraw_version;
+        
+    public:
+        deRawModule();
+        virtual ~deRawModule();
+
+        void onInit();
+
+        std::string getVersion() const;
+        bool loadRAW(const std::string& fileName, deStaticImage& image);
+
+
+};
+
+#endif
