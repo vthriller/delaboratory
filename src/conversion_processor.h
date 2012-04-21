d64e9efd199b757e464ea67d8ce9674381d5a4ec
jacekpoplawski <jacekpoplawski@gmail.com>
2012-04-21 17:58:32 +0000

    use conversion processor in color panel

--- src/conversion_processor.h
+++ src/conversion_processor.h
@@ -35,6 +35,7 @@ public:
     void convertImage(const deImage& sourceImage, deImage& image, deChannelManager& channelManager);
     bool renderImageToRGB(const deImage& image, unsigned char* data, deChannelManager& channelManager);
     bool convertPixel(const deImage& image, int p, deColorSpace colorSpace, deValue &v1, deValue& v2, deValue& v3, deValue& v4);
+    bool convertToRGB(deValue v1, deValue v2, deValue v3, deValue v4, deColorSpace colorSpace, deValue &rr, deValue& gg, deValue& bb);
 
 };
 
