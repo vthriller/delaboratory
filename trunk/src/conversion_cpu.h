7fd791755063779befcee288eaeac66686ae1703
jacekpoplawski@gmail.com <jacekpoplawski@gmail.com@4e2834a2-6c7f-4a28-f73c-28c74d6238bb>
2012-05-03 22:23:06 +0000

    conversion cpu

---
+++ trunk/src/conversion_cpu.h
@@ -0,0 +1,77 @@
+/*
+    delaboratory - color correction utility
+    Copyright (C) 2011 Jacek Poplawski
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _DE_CONVERSION_CPU_H
+#define _DE_CONVERSION_CPU_H
+
+#include "value.h"
+class deImage;
+class deChannelManager;
+#include "color_space.h"
+
+#define CPU_REGISTER_OVERFLOW 0
+#define CPU_REGISTER_CMYK_KEY_SUB 1
+#define CPU_REGISTER_CMYK_KEY_MAX 2
+#define CPU_REGISTER_CMYK_MIN_SUM 3
+#define CPU_REGISTER_BW_MIXER_R 4
+#define CPU_REGISTER_BW_MIXER_G 5
+#define CPU_REGISTER_BW_MIXER_B 6
+#define CPU_REGISTER_CONTRAST 7
+#define CPU_REGISTER_SATURATION 8
+
+#define CPU_REGISTERS 9
+
+class deConversionCPU
+{
+    private:
+    public:
+        typedef void (*deFunction)(deConversionCPU& cpu);
+
+        deValue* input;
+        deValue* output;
+        deValue* registers;
+
+        deConversionCPU(int size);
+        virtual ~deConversionCPU();
+
+        void switchIO();
+        void incOverflow();
+
+        void convertImage3x3(const deImage& sourceImage, deImage& image, deFunction f1, deFunction f2);
+        void convertImage3x4(const deImage& sourceImage, deImage& image, deFunction f1, deFunction f2);
+        void convertImage4x3(const deImage& sourceImage, deImage& image, deFunction f1, deFunction f2);
+        void convertImage3x1(const deImage& sourceImage, deImage& image, deFunction f1, deFunction f2);
+        void convertImage1x3(const deImage& sourceImage, deImage& image, deFunction f1, deFunction f2);
+
+        bool renderImage1(const deImage& image, deConversionCPU::deFunction f, unsigned char* data);
+        bool renderImage3(const deImage& image, deConversionCPU::deFunction f, unsigned char* data);
+        bool renderImage4(const deImage& image, deConversionCPU::deFunction f, unsigned char* data);
+
+};
+
+void rgb2cmy(deConversionCPU& cpu);
+void cmy2rgb(deConversionCPU& cpu);
+void cmy2cmyk(deConversionCPU& cpu);
+void cmyk2cmy(deConversionCPU& cpu);
+void rgb2prophoto(deConversionCPU& cpu);
+void prophoto2rgb(deConversionCPU& cpu);
+void empty(deConversionCPU& cpu);
+
+deConversionCPU::deFunction getConversion(deColorSpace sourceColorSpace, deColorSpace targetColorSpace);
+
+#endif
--- trunk/src/conversion_processor.h
+++ trunk/src/conversion_processor.h
@@ -19,51 +19,7 @@
 #ifndef _DE_CONVERSION_PROCESSOR_H
 #define _DE_CONVERSION_PROCESSOR_H
 
-#include "value.h"
-class deImage;
-class deChannelManager;
-#include "color_space.h"
-
-#define CPU_REGISTER_OVERFLOW 0
-#define CPU_REGISTER_CMYK_KEY_SUB 1
-#define CPU_REGISTER_CMYK_KEY_MAX 2
-#define CPU_REGISTER_CMYK_MIN_SUM 3
-#define CPU_REGISTER_BW_MIXER_R 4
-#define CPU_REGISTER_BW_MIXER_G 5
-#define CPU_REGISTER_BW_MIXER_B 6
-#define CPU_REGISTER_CONTRAST 7
-#define CPU_REGISTER_SATURATION 8
-
-#define CPU_REGISTERS 9
-
-
-class deConversionCPU
-{
-    private:
-    public:
-        typedef void (*deFunction)(deConversionCPU& cpu);
-
-        deValue* input;
-        deValue* output;
-        deValue* registers;
-
-        deConversionCPU(int size);
-        virtual ~deConversionCPU();
-
-        void switchIO();
-        void incOverflow();
-
-        void convertImage3x3(const deImage& sourceImage, deImage& image, deFunction f1, deFunction f2);
-        void convertImage3x4(const deImage& sourceImage, deImage& image, deFunction f1, deFunction f2);
-        void convertImage4x3(const deImage& sourceImage, deImage& image, deFunction f1, deFunction f2);
-        void convertImage3x1(const deImage& sourceImage, deImage& image, deFunction f1, deFunction f2);
-        void convertImage1x3(const deImage& sourceImage, deImage& image, deFunction f1, deFunction f2);
-
-        bool renderImage1(const deImage& image, deConversionCPU::deFunction f, unsigned char* data);
-        bool renderImage3(const deImage& image, deConversionCPU::deFunction f, unsigned char* data);
-        bool renderImage4(const deImage& image, deConversionCPU::deFunction f, unsigned char* data);
-
-};
+#include "conversion_cpu.h"
 
 
 class deConversionProcessor
@@ -85,12 +41,5 @@ class deConversionProcessor
 };
 
 
-
-void rgb2cmy(deConversionCPU& cpu);
-void cmy2rgb(deConversionCPU& cpu);
-void cmy2cmyk(deConversionCPU& cpu);
-void cmyk2cmy(deConversionCPU& cpu);
-void rgb2prophoto(deConversionCPU& cpu);
-void prophoto2rgb(deConversionCPU& cpu);
 
 #endif
